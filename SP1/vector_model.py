f19e194566bf622adf65c7283e81378cf5880e4d
janburian <jburian@students.zcu.cz>
2023-03-20 17:31:06 +0100

    Added method to reformat queries

--- SP1/vector_model.py
+++ SP1/vector_model.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-import xml.dom.minidom
 
 def load_documents(path_to_directory: Path):
     res = []
@@ -18,14 +17,59 @@ def load_queries(filename: str):
     res = []
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
-            line_strip = line.rstrip("\n")
-            res.append(line_strip)
-            # list_final_phonemes.append(list_transcript)
+            if line == '' or line == '\n' or line == ' \n':
+                continue
+            else:
+                line_strip = line.rstrip("\n")
+                res.append(line_strip)
+
+    return res
+
+def reformat_queries(queries: list):
+    DOC_start_tags_indices = []
+    DOC_end_tags_indices = []
+    idx = 0
+
+    for element in queries:
+        if element == '<DOC>':
+            DOC_start_tags_indices.append(idx)
+        elif element == '</DOC> ' or element == '</DOC>':
+            DOC_end_tags_indices.append(idx)
+
+        idx += 1
+
+    separated_query_texts = separate_query_text(DOC_end_tags_indices, DOC_start_tags_indices, queries)
+
+    return separated_query_texts
+
+
+def separate_query_text(DOC_end_tags_indices, DOC_start_tags_indices, queries):
+    dict_queries = {}
+    for i in range(len(DOC_start_tags_indices)):
+        topic = queries[DOC_start_tags_indices[i] + 1]  # element on next index after <DOC> is <DOCNO>number<\DOCNO>
+        topic_identifier = int(''.join(filter(str.isdigit, topic)))
+        extracted_text_start = (DOC_start_tags_indices[i] + 2)
+        extracted_text_end = DOC_end_tags_indices[i]
+        extracted_text_list = queries[extracted_text_start: extracted_text_end]  # text separation
+        extracted_text_str = ' '.join(extracted_text_list)
+        dict_queries[topic_identifier] = extracted_text_str
+
+    return dict_queries
+
+
+def reformat_documents(documents_list):
+    pass
 
 
 if __name__ == '__main__':
     path_to_documents_directory = Path("./documents")
     queries_filename = "query_devel.xml"
 
+    # Loading documents
     documents_list = load_documents(path_to_documents_directory)
-    load_queries(queries_filename)
+    queries_list = load_queries(queries_filename)
+
+    # Reformatting
+    queries_final = reformat_queries(queries_list)
+
+    print()
